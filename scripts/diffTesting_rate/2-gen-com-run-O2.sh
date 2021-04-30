6466bc1e1f69d8006dc66a1f3173a2d6322a5278
Karine EM <karineven79@gmail.com>
2021-04-30 16:57:33 +0100

    Update 2-gen-com-run-O2.sh

--- scripts/diffTesting_rate/2-gen-com-run-O2.sh
+++ scripts/diffTesting_rate/2-gen-com-run-O2.sh
@@ -1,12 +1,9 @@
 #!/bin/bash 
-i=$1
-baseD=/home/user42
-base=$baseD/RRS_EXPR # $baseD/git/RRS_EXPR
-nb_progs_to_gen=50000
+baseD=$1		# e.g., /home/user42
+seeds=$2		# e.g., "../../Data/seeds_all/seeds_out1.txt"
 
 ######################################## Start Cov. ########################################
 # Loop over compilation and coverage measurement
-seeds="../../Data/seeds_all/seeds_out$i.txt"
 while IFS= read -r seed
 do
 	## Generate
