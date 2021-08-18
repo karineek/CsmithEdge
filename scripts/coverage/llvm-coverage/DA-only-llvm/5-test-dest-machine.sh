b28b139a6843116485f36c2d1932e5dd53698145
Karine EM <karineven79@gmail.com>
2021-08-18 16:45:31 +0100

    Update 5-test-dest-machine.sh

--- scripts/coverage/llvm-coverage/DA-only-llvm/5-test-dest-machine.sh
+++ scripts/coverage/llvm-coverage/DA-only-llvm/5-test-dest-machine.sh
@@ -1,38 +1,39 @@
 #!/bin/bash 
-process_number=$1
-baseD=/home/user42
-working_folder=$baseD/llvm-csmith-1 ##$process_number
-nb_progs_to_gen=4 #100000
-nb_progs_to_gen_per_step=2 #20000
-#seed_location=../../Data/seeds_all/seeds_out$process_number.txt # seed location
-
-base=$baseD/RRS_EXPR # $baseD/git/RRS_EXPR
-seed_location=$base/scripts/WA-v2-standalone/seeds/seeds-$process_number.txt	# seed location
-csmith_location=$base/csmith							# csmith location
-# csmith_exec=$base/csmith/build/src/csmith					# csmith exec
-configuration_location=$working_folder/csmith/scripts/compiler_test.in		# config file locatoin
-outputs_location=../../ # where we will put all outputs
-
-## Change per test:
-#csmith_flags=" --bitfields --packed-struct " #" --bitfields --packed-struct --math-notmp --annotated-arith-wrappers" 
-
-## 0
-modify=0 # 0-original, 1-modify
-##compile_line=" -I$csmith_location/runtime -I$csmith_location/build/runtime -lgcov -w" #how we compile it,modify=0
-compile_line=" -I$csmith_location/runtime -I$csmith_location/build/runtime -lgcov -w -DUSE_MATH_MACROS" #how we compile it,modify=0
-csmith_flags=" --bitfields --packed-struct " #" --bitfields --packed-struct --math-notmp" 
-
-## 1
-#modify=1 # 0-original, 1-modify
-##compile_line=" -I../../csmith_runtime_orig -I../../csmith_runtime_orig_build -lgcov -w" #how we compile it,modify=1
-#compile_line=" -I$csmith_location/RRS_runtime_test -I$csmith_location/build/runtime -DUSE_MATH_MACROS -lgcov -w" #how we compile it,modify=1
-#csmith_flags=" --bitfields --packed-struct --annotated-arith-wrappers" #" --bitfields --packed-struct --math-notmp" 
+process_number=$1		# Seeds set number
+working_folder=$2		# compiler installed with coverage
+project_folder=$3		# project folder location (where CsmithEdge is)
+nb_progs_to_gen=$4		#100000
+nb_progs_to_gen_per_step=$5	#20000
+modify=$6			# 0 - regular Csmith, 1 - CsmithEdge
 
 ## Check we have process number
 if [ -z "$1" ]
   then
-	echo "No process number supplied."
-	exit 1
+        echo "No process number supplied."
+        exit 1
+fi
+
+base=$project_folder
+seed_location=$base/Data/seeds/seeds-$process_number.txt 		# seed location
+csmith_location=$base/csmith						# csmith location
+wda_folder=$base/AE/Data/set-3/WDA/					# location of dynamic anlysis results for this set
+configuration_location=$working_folder/csmith/scripts/compiler_test.in	# config file locatoin
+outputs_location=../../../ # where we will put all outputs
+
+## Change per test:
+#csmith_flags=" --bitfields --packed-struct " #" --bitfields --packed-struct --math-notmp --annotated-arith-wrappers" 
+if [[ "$modify" == "1" ]] ; then
+	echo "Get seed list from $seed_location <CsmithEdge>"
+	## modify=1 : 0-original, 1-modify
+	compile_line=" -I$csmith_location/runtime -I$csmith_location/build/runtime -lgcov -w" #how we compile it,modify=0
+	##compile_line=" -I$csmith_location/runtime -I$csmith_location/build/runtime -lgcov -w -DUSE_MATH_MACROS" #how we compile it,modify=0
+	csmith_flags=" --bitfields --packed-struct " #" --bitfields --packed-struct --math-notmp" 
+else
+	echo "Get seed list from $seed_location <Csmith>"
+	## modify=0 : 0-original, 1-modify
+	##compile_line=" -I../../csmith_runtime_orig -I../../csmith_runtime_orig_build -lgcov -w" #how we compile it,modify=1
+	compile_line=" -I$csmith_location/RRS_runtime_test -I$csmith_location/build/runtime -DUSE_MATH_MACROS -lgcov -w" #how we compile it,modify=1
+	csmith_flags=" --bitfields --packed-struct --annotated-arith-wrappers" #" --bitfields --packed-struct --math-notmp" 
 fi
 
 time1=$(date +"%T")
@@ -70,7 +71,7 @@ do
 	# Run compiler and save coverage data
  	export GCOV_PREFIX=$working_folder/coverage_gcda_files/application_run-$modify
 	export GCOV_PREFIX_STRIP=0
- 	./RRS5_1-compiler_test.sh $process_number $nb_gen_progs $nb_progs_to_gen_per_step $modify $seed_location $csmith_location $configuration_location $outputs_location "$compile_line" "$csmith_flags"
+ 	./RRS5_1-compiler_test.sh $process_number $nb_gen_progs $nb_progs_to_gen_per_step $modify $seed_location $csmith_location $configuration_location $outputs_location "$compile_line" "$csmith_flags" $wda_folder
 	unset GCOV_PREFIX
 	unset GCOV_PREFIX_STRIP
 		
@@ -80,7 +81,7 @@ do
 	(
 		cd $working_folder/coverage_processed-$modify/x-$i
 		source $baseD/graphicsfuzz/gfauto/.venv/bin/activate
-		gfauto_cov_from_gcov --out run_gcov2cov.cov $working_folder/llvm-build/ $working_folder/coverage_gcda_files/application_run-$modify/ --num_threads 32 --gcov_uses_json >> gfauto.log 2>&1 
+		gfauto_cov_from_gcov --out run_gcov2cov.cov $working_folder/llvm-build/ --gcov_prefix_dir $working_folder/coverage_gcda_files/application_run-$modify/ --num_threads 32 --gcov_uses_json >> gfauto.log 2>&1 
 		gfauto_cov_to_source --coverage_out cov.out --cov run_gcov2cov.cov  $working_folder/llvm-build/ >> gfauto.log 2>&1 
 	)
 	cd $current_folder
