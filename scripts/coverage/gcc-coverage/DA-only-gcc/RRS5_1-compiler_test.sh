3eb5f14380dfd593659d8c84efb8041317a55db3
Karine EM <karineven79@gmail.com>
2021-08-18 15:37:24 +0100

    Update RRS5_1-compiler_test.sh

--- scripts/coverage/gcc-coverage/DA-only-gcc/RRS5_1-compiler_test.sh
+++ scripts/coverage/gcc-coverage/DA-only-gcc/RRS5_1-compiler_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 process_number=$1						# Process number
-nb_gen_progs=$2						# How many we will do by the end of the loop
+nb_gen_progs=$2							# How many we will do by the end of the loop
 nb_progs_to_gen_per_step=$3					# How many we shall generate now
 initc=$(($nb_gen_progs + 1 - $nb_progs_to_gen_per_step)) 	# Calc. the position of the first seed of this batch
 modify=$4							# With modifications --> use 1
@@ -8,11 +8,12 @@ seed_location=$5						# Seeds file location, coverage takes good+bad
 csmith_location=$6						# Where is the csmith we use
 compconfg=$7							# Where to take the compiler configuration
 outputs_location=$8						# Where to expect the results
-compile_line=$9						# Include and -D params for testcase
+compile_line=$9							# Include and -D params for testcase
 csmith_flags=${10}						# csmith flags
+wda_folder=${11}						# location of weaken dynamic anlaysis results for these seeeds
 
 ### Check we have all the data we need before proceeding:
-if [ "$#" -ne 10 ]; then
+if [ "$#" -ne 11 ]; then
 	echo "Illegal number of parameters. Please enter compiler and number of testcases to generate."
 	echo "Requires: process number, size of population, size of population per step, flag (1-modify, 0-original csmith) seeds file, csmith location, configuration file location, output location, compilation line for test case, and csmith flags."
 	exit 1
