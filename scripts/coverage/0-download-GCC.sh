67c69c6a7ee12383300a4d88fc3f06c152dc5ceb
Karine EM <karineven79@gmail.com>
2021-08-31 09:52:00 +0100

    Update 0-download-GCC.sh

--- scripts/coverage/0-download-GCC.sh
+++ scripts/coverage/0-download-GCC.sh
@@ -2,6 +2,7 @@
 shopt -s extglob # Activate extended pattern matching in bash
 
 working_folder=$1
+version=$2
 
 sudo apt-get install make
 sudo apt-get install flex -y
@@ -12,9 +13,14 @@ TMP_SOURCE_FOLDER=$(mktemp -d $working_folder/.sources.XXXXXXX.tmp)
 cd $TMP_SOURCE_FOLDER
 ## Get gcc
 git clone git://gcc.gnu.org/git/gcc.git
-cd gcc
-git checkout releases/gcc-10
-cd ..
+if [ -z "$2" ] ; then
+	echo " >> Checking out master branch"
+else
+	echo " >> Checking out releases/gcc-$version tag"
+  cd gcc
+	git checkout releases/gcc-"$version"
+  cd ..
+fi
 ## Get pre-req. isl-0.18.tar.bz2;gmp-6.1.0.tar.bz2;mpc-1.0.3.tar.gz; mpfr-3.1.4.tar.bz2 => works for gcc-10
 ## GMP
 wget ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-6.1.0.tar.bz2
