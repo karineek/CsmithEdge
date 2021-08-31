0c0b3b7b033cae3ec6fc29c0e56f7a83dd6d8068
Karine EM <karineven79@gmail.com>
2021-08-31 09:50:01 +0100

    Update 0-download-LLVM.sh

--- scripts/coverage/0-download-LLVM.sh
+++ scripts/coverage/0-download-LLVM.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 working_folder=$1
+version=$2
+if [ -z "$version" ] ; then
+	version=11
+fi
 
 # Downloading LLVM and Csmith sources, building Csmith
 TMP_SOURCE_FOLDER=$(mktemp -d $working_folder/.sources.XXXXXXX.tmp)
 cd $TMP_SOURCE_FOLDER
 ## LLVM
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/llvm-11.0.0.src.tar.xz
-tar -xvf llvm-11.0.0.src.tar.xz
-rm llvm-11.0.0.src.tar.xz
-mv llvm-11.0.0.src llvm-source
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-$version.0.0/llvm-$version.0.0.src.tar.xz
+tar -xvf llvm-$version.0.0.src.tar.xz
+rm llvm-$version.0.0.src.tar.xz
+mv llvm-$version.0.0.src llvm-source
 cd ./llvm-source/tools
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang-11.0.0.src.tar.xz
-tar -xvf clang-11.0.0.src.tar.xz
-rm clang-11.0.0.src.tar.xz
-mv clang-11.0.0.src clang
-echo ">> Downloading LLVM 11 source filese ($TMP_SOURCE_FOLDER)"
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-$version.0.0/clang-$version.0.0.src.tar.xz
+tar -xvf clang-$version.0.0.src.tar.xz
+rm clang-$version.0.0.src.tar.xz
+mv clang-$version.0.0.src clang
+echo ">> Downloading LLVM $version source filese ($TMP_SOURCE_FOLDER)"
