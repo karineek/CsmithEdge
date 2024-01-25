8c5966b48f09e7470debc5e911387a155ac37d72
Karine EM <karineven79@gmail.com>
2024-01-25 22:28:53 +0000

    Update gen_and_diff_testing_csmithedge.sh

--- AE/gen_and_diff_testing_csmithedge.sh
+++ AE/gen_and_diff_testing_csmithedge.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-base=$1		# E.g, /home/user42/git/CsmithEdge
-lazy=$2		# 0 - Reg. and 1 - lazy
-date_str=09082021	# Date for our experiments, does not change the results if you keep it that way.
+base=$1			# E.g, /home/user42/git/CsmithEdge
+lazy=$2			# 0 - Reg. and 1 - lazy
+date_str=09082021	# Date for our experiments does not change the results if you keep it that way.
 compA=$3
 compB=$4
 
 ## Check we have two parameters
 if [ "$#" -ne 4 ]; then
-	echo "Illegal number of parameters. Please enter base folder and 0 or 1 (csmithedge or lazy)."
+	echo "Illegal number of parameters. Please enter the base folder and 0 or 1 (csmithedge or lazy)."
 	exit 1
 fi
 
 ## Check the folder exists
 if [ ! -d $base/scripts/CsmithEdge ] ; then
-	echo "Cannot find base folder. No such path: <$base/scripts/CsmithEdge>"
+	echo "Cannot find the base folder. No such path: <$base/scripts/CsmithEdge>"
 	exit
 fi
 
